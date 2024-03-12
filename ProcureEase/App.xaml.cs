958c81591215cfbb3d641e09bdec3bac3728cb82
nodecspro <nowickow.artem2015@yandex.ru>
2024-03-12 13:24:18 +0300

    Create project and auth form

---
+++ ProcureEase/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProcureEase
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ProcureEase/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProcureEase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Вывод сообщения об успешной авторизации
+            MessageBox.Show("Вы успешно авторизовались!", "Авторизация", MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
